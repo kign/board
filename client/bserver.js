4e0bdecaa787ebca81d63c4dda3261fac081d0b1
Konstantin Ignatiev <kostya@gmail.com>
2020-03-04 21:57:32 -0500

    Added server sprite utilities

--- client/bserver.js
+++ client/bserver.js
@@ -9,9 +9,10 @@ const bserver = (function () {
 		bcontrol.refresh ();
 	}
 
-	const server_error(xhr) {
+	const server_error = function (api, xhr) {
 		if (xhr)
-			console.error("readyState =",      xhr.readyState,
+			console.error("api = ",            api,
+				          "; readyState =",    xhr.readyState,
 				          ";  status =",       xhr.status,
 			    	      ";  responseText =", xhr.responseText.substring(0,80));
 
@@ -26,9 +27,10 @@ const bserver = (function () {
 		handshake: function (report_result) {
 			const xhr  = new XMLHttpRequest();
 			bcontrol.set_server_status("testing");
+			const api = '/timetest';
 			xhr.open('GET',
 				     base_url +
-				        '/timetest' + "?" + (new URLSearchParams({
+				        api + "?" + (new URLSearchParams({
 						 	clientUTCtime: (new Date()).valueOf()
 			})),
 				     true);
@@ -38,7 +40,7 @@ const bserver = (function () {
 					console.log("Result", res);
 					set_server_online(true);
 				} else
-					server_error(xhr);
+					server_error(api, xhr);
 			}
 			xhr.onerror = server_error;
 			xhr.send();
@@ -46,7 +48,8 @@ const bserver = (function () {
 
 		sync_blocks: function(blocks, onUpdate) {
 			const xhr  = new XMLHttpRequest();
-			xhr.open('POST', base_url + "/blocks", true);
+			const api = "/blocks";
+			xhr.open('POST', base_url + api, true);
 			xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
 			xhr.onload = function () {
 				if (xhr.readyState == 4 && xhr.status == "200") {
@@ -54,24 +57,63 @@ const bserver = (function () {
 					console.log("Result", res);
 					onUpdate(res);
 				} else
-					server_error(xhr);
+					server_error(api, xhr);
 			}
 			xhr.onerror = server_error;
 			xhr.send(JSON.stringify(blocks));
 		},
 
-		reset: function(blocks, onUpdate) {
+		reset: function(onUpdate) {
 			const xhr  = new XMLHttpRequest();
-			xhr.open('POST', base_url + "/reset", true);
+			const api = "/reset";
+			xhr.open('POST', base_url + api, true);
 			xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
 			xhr.onload = function () {
-				if (xhr.readyState == 4 && xhr.status == "200") {
-					onUpdate();
+				if (xhr.readyState == 4 && [200, 204].includes(xhr.status)) {
+					if (onUpdate)
+						onUpdate();
 				} else
-					server_error(xhr);
+					server_error(api, xhr);
 			}
 			xhr.onerror = server_error;
 			xhr.send();
+		},
+
+		get_sprite: function(onUpdate) {
+			const xhr  = new XMLHttpRequest();
+			const api = "/get-sprite";
+			xhr.open('GET', base_url + api, true);
+			xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+			xhr.onload = function () {
+				if (xhr.readyState == 4 && xhr.status == "200") {
+					const res = JSON.parse(xhr.responseText);
+					if (onUpdate)
+						onUpdate(res.x, res.y);
+				}
+				else if (xhr.readyState == 4 && xhr.status == "204") {
+					if (onUpdate)
+						onUpdate(null, null);
+				} else
+					server_error(api, xhr);
+			}
+			xhr.onerror = server_error;
+			xhr.send();
+		},
+
+		put_sprite: function(x, y, onUpdate) {
+			const xhr  = new XMLHttpRequest();
+			const api = "/put-sprite";
+			xhr.open('POST', base_url + api, true);
+			xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+			xhr.onload = function () {
+				if (xhr.readyState == 4 && [200, 204].includes(xhr.status)) {
+					if (onUpdate)
+						onUpdate();
+				} else
+					server_error(api, xhr);
+			}
+			xhr.onerror = server_error;
+			xhr.send(JSON.stringify({x: x, y: y}));
 		}
 	};
 })();
--- server/board.py
+++ server/board.py
@@ -3,15 +3,20 @@ from model import Block
 from pprint import pprint
 
 def api_blocks(clientBlocks) :
-	print(f'{len(clientBlocks)} incoming blocks')
-	pprint(clientBlocks)
+	verbose = False
+
+	if verbose :
+		print(f'{len(clientBlocks)} incoming blocks')
+		pprint(clientBlocks)
 
 	N = 10000 # max possible Y size
 	blocks = {sb.x * N + sb.y : sb for sb in g.session.query(Block)}
 	for sb in blocks.values() :
 		sb.push = True
-	print(f'{len(blocks)} database blocks')
-	pprint(blocks)
+
+	if verbose :
+		print(f'{len(blocks)} database blocks')
+		pprint(blocks)
 
 	for cb in clientBlocks :
 		ii = cb['x'] * N + cb['y']
@@ -26,15 +31,32 @@ def api_blocks(clientBlocks) :
 			g.session.add(sb)
 			sb.push = False
 
-
 	res = [sb.to_json() for sb in blocks.values() if sb.push]
-	print(f'Returning {len(res)} blocks')
-	pprint(res)
 
-	print("Comitting")
+	if verbose :
+		print(f'Returning {len(res)} blocks')
+		pprint(res)
+
+	if verbose :
+		print("Comitting")
 	g.session.commit()
 
 	return res
 
 def api_reset() :
 	g.session.query(Block).delete();
+
+
+def api_put_sprite(sprite) :
+	print(f"Saving sprite({sprite['x']},{sprite['y']})")
+	g.cache.set('sprite', sprite)
+
+def api_get_sprite() :
+	sprite = g.cache.get('sprite')
+	if sprite :
+		print(f"Returning sprite({sprite['x']},{sprite['y']})")
+		return sprite
+	else :
+		print("No sprite saved")
+		return None
+
